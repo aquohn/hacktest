0b15f541ca3416dd13f2aa563985e0137733a072
John Khoo <johnkhootf@gmail.com>
2025-04-27 15:30:01 +0800

    Add example config files

---
+++ example_config.py
@@ -0,0 +1,8 @@
+
+TICKERS = ["CSPX.L", "VUAA.L", "VWRA.L", "IWDA.L", "SGOV"]
+FMP_PATH = "Data/FMP/"
+AV_PATH = "Data/AV/"
+
+TO_COMPARE = [
+    "CSPX.L", "VWRA.L", "IWDA.L", "VUAA.L", # global indices
+]
